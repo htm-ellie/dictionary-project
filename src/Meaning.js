27d2663d497b2a6b0a20749a2dc06afced4b1708
htm-ellie <html.elitt@gmail.com>
2026-04-02 14:47:52 -0400

    rendered the meanings

---
+++ src/Meaning.js
@@ -0,0 +1,18 @@
+import Reach from "react";
+
+export default function Meaning(props) {
+  console.log(props.meaning);
+  return (
+    <div className="Meaning">
+      <h5> {props.meaning.partOfSpeech}</h5>
+
+      <div className="Meaning">
+        <p>
+          {props.meaning.definition}
+          <br />
+          <em>{props.meaning.example}</em>
+        </p>
+      </div>
+    </div>
+  );
+}
